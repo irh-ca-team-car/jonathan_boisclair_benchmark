493204bedc173f0c99e20f291d88e30d51d2a351
Jonathan Boisclair <jonathan.boisclair@uqtr.ca>
2023-01-11 17:11:41 -0500

    __

--- setup.bash
+++ setup.bash
@@ -1,13 +1,15 @@
-if [ -e interface/datasets/coco/annotations ] 
-then
-    echo "Already setup"
-else
-    wget https://ia600904.us.archive.org/21/items/MSCoco2014/annotations_trainval2014.zip
-    mkdir -p interface/datasets/coco
-    mv annotations_trainval2014.zip interface/datasets/coco
-    cd interface/datasets/coco
-    unzip annotations_trainval2014.zip
-fi
+#if [ -e interface/datasets/coco/annotations ] 
+#then
+#    echo "Already setup"
+#else
+#    wget https://ia600904.us.archive.org/21/items/MSCoco2014/annotations_trainval2014.zip
+#    mkdir -p interface/datasets/coco
+#    mv annotations_trainval2014.zip interface/datasets/coco
+#    cd interface/datasets/coco
+#    unzip annotations_trainval2014.zip
+#fi
 
-npm install pad-left
-node convert-flir.mjs
+#npm install pad-left
+#node convert-flir.mjs
+
+pip3 install torch torchvision torchaudio --upgrade
