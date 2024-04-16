076e6eb42a51c3e42d7b03f1c42a10ad25b290c8
Jonathan Boisclair <jonathan.boisclair@uqtr.ca>
2024-04-16 14:42:32 -0400

    n

--- interface/datasets/detection/VoC2007.py
+++ interface/datasets/detection/VoC2007.py
@@ -3,7 +3,6 @@ from .. import Sample, LidarSample
 from ...detectors.Detection import Detection, Box2d, Box3d
 import fiftyone as fo
 import fiftyone.zoo as foz
-
 class VoC2007Detection:
     A1Classes = None
     images: fo.Dataset
