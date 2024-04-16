dd753c46e4d1f080f87b2095f54252e766ceeff0
Jonathan Boisclair <jonathan.boisclair@uqtr.ca>
2024-04-16 15:11:39 -0400

    f

--- interface/datasets/detection/__init__.py
+++ interface/datasets/detection/__init__.py
@@ -3,7 +3,7 @@ from .A1 import A1Detection
 from .A2 import A2Detection
 from .Citiscapes import CitiscapesDetection
 from .Coco import CocoDetection
-#from .VoC2007 import VoC2007Detection
+from .VoC2007 import VoC2007Detection
 from .A2W import A2W
 
 DetectionDataset.register("coco-empty",CocoDetection())
